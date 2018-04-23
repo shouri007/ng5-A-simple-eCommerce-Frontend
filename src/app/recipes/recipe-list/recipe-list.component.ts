725814f55a22454a336fa2cac4150cf58d1f9b1f
shouri007 <shouri.piratla9@gmail.com>
2018-04-23 08:52:48 +0530

    Added list view and components for recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test', 'http://kusaka.co/wordpress/wp-content/uploads/2016/02/Avocado-Toast-Recipe-Step-by-Step-11.jpg'),
+      new Recipe('A Test Recipe','This is simply a test', 'http://kusaka.co/wordpress/wp-content/uploads/2016/02/Avocado-Toast-Recipe-Step-by-Step-11.jpg')
+  ];
 
   constructor() { }
 
