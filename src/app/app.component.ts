0b2e61a1d267c44b7c887ccd7e288fcc7cebd0ca
shouri007 <shouri.piratla9@gmail.com>
2018-04-25 07:17:08 +0530

    Implement routing using event emitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
+  loadedFeature = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+    console.log(this.loadedFeature);
+  }
 }
